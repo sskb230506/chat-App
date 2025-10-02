01383331b7a93509c576482fb1866b426bf93a45
sskb230506 <sskb230506@gmail.com>
2025-10-02 16:19:17 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoute from "./routes/auth.routes.js";
 
 dotenv.config();
@@ -8,7 +9,15 @@ const port=process.env.PORT ||3000;
 
 
 const app=express();
+const __dirname=path.resolve();
+
 app.use("/api/auth",authRoute);
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.use((_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    })
+}
 app.listen(port,()=>{
     console.log(`App is listening on port ${port}`);
 })
